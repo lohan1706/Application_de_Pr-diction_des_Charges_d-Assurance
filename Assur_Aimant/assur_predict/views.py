7c2b67bcd47cffe27012f14b741710db84372a19
Edilene <eassuncaosilva@gmail.com>
2026-01-27 17:09:08 +0100

    add: first user profile settings

--- Assur_Aimant/assur_predict/views.py
+++ Assur_Aimant/assur_predict/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ProfileForm
+
+@login_required
+def profile_view(request):
+    profile = request.user.profile
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, "assur_predict/profile.html", {
+        "form": form
+    })
